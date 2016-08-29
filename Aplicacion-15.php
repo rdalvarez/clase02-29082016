815e1c327282a2ffe999c9f9ec88392d457a44ce
rdalvarez <david_de_odin@hotmail.com>
2016-08-29 20:10:05 -0300

    creando el contructor, los metodos y variables

---
+++ Aplicacion-15.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+	<title></title>
+</head>
+<body> 
+<?php
+
+Class FiguraGeometrica
+{
+	protected $_color;
+
+	function __contruct()
+	{
+
+	}
+
+	function getColor()
+	{
+		return $this->_color;
+	}
+
+	function setColor($var)
+	{
+		$this->_color = $var;
+	}
+
+	
+
+}
+
+?>
+</body>
+</html>
